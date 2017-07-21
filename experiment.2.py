5e3119e4dbc949b6c7dc46a130f157949edd0fae
Richard Sharp <rsharp@starbucks.com>
2017-07-21 15:36:31 -0500

    multisegment, multioffer, one month experiment with appropriate sensitivities

--- experiment.2.py
+++ experiment.2.py
@@ -5,10 +5,9 @@ import unittest
 
 import os.path
 import glob
-import shutil
-import json
 import datetime
 import numpy
+import shutil
 
 from externalities import World, Offer, Transaction, Event, Categorical
 from person import Person
@@ -17,41 +16,153 @@ from population import Population
 dt_fmt = '%Y%m%d'
 now = datetime.datetime.strptime('20170718', dt_fmt)
 
-def create_people(n):
-    dob_optout_rate = 0.1
+def create_people_0(n):
+    profile_optout_rate = 0.1
+    min_tenure = 0
+    max_tenure = 180
+    mean_age = 365*30
+    std_age = 365*2
+    gender_rates = [0.49, 0.49, 0.02]
+    min_income = 50000
+    max_income = 75000
+    beta = 1.0 / 0.0004
+    g = lambda x: 1.0 / (1.0 + numpy.exp(-x))
+    g_inv = lambda y: numpy.log(y / (1.0 - y))
 
     people = list()
     for i in range(n):
-        became_member_on = (now - datetime.timedelta(days=numpy.random.choice(range(700)))).strftime(dt_fmt)
+        became_member_on = (now - datetime.timedelta(days=numpy.random.choice(range(min_tenure, max_tenure)))).strftime(dt_fmt)
 
-        if numpy.random.random() < 1.0 - dob_optout_rate:
-            dob = (now - datetime.timedelta(days=numpy.random.poisson(365*25))).strftime(dt_fmt)
+        if numpy.random.random() < 1.0 - profile_optout_rate:
+            # must be at least 18 to join
+            dob = (now - datetime.timedelta(days=int(max(365.25*18, (numpy.random.normal(mean_age, std_age)))))).strftime(dt_fmt)
+            # three values + missing
+            gender = numpy.random.choice(['M', 'F', 'O'], p=gender_rates)
+            income = None
+            for i in range(25):
+                x = max(25000, numpy.random.exponential(beta))
+                if min_income <= x <= max_income:
+                    income = x
+                    break
         else:
             dob = None
+            gender = None
+            income = None
+
+        person_view_offer_sensitivity = Categorical(['background', 'offer_age', 'web', 'email', 'mobile', 'social'],
+                                                    [g_inv(0.20) - 4, -abs(g_inv(0.01) / float(1 * 24 * 30)), 0, 1, 1,
+                                                     2])
+        person_make_purchase_sensitivity = Categorical(
+            ['background', 'time_since_last_transaction', 'last_viewed_offer_strength', 'viewed_active_offer'],
+            [g_inv(1.0 / 24.0), abs(g_inv(0.10) / float(1 * 24 * 30)), 1, 1])
+        person_purchase_amount_sensitivity = Categorical(
+            ['background', 'income_adjusted_purchase_sensitivity', 'front page', 'local', 'entertainment', 'sports',
+             'opinion', 'comics', 'sweet', 'sour', 'salty', 'bitter', 'umami'], [1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0])
 
         people.append(Person(became_member_on,
-                             dob=dob))
+                             dob=dob,
+                             gender=gender,
+                             income=income,
+                             view_offer_sensitivity=person_view_offer_sensitivity,
+                             make_purchase_sensitivity=person_make_purchase_sensitivity,
+                             purchase_amount_sensitivity=person_purchase_amount_sensitivity))
+
+    return people
+
+
+def create_people_1(n):
+    profile_optout_rate = 0.2
+    min_tenure = 60
+    max_tenure = 540
+    mean_age = 365*35
+    std_age = 365*2
+    gender_rates = [0.49, 0.49, 0.02]
+    min_income = 30000
+    max_income = 60000
+    beta = 1.0 / 0.0004
+    g = lambda x: 1.0 / (1.0 + numpy.exp(-x))
+    g_inv = lambda y: numpy.log(y / (1.0 - y))
+
+    people = list()
+    for i in range(n):
+        became_member_on = (now - datetime.timedelta(days=numpy.random.choice(range(min_tenure, max_tenure)))).strftime(dt_fmt)
+
+        if numpy.random.random() < 1.0 - profile_optout_rate:
+            # must be at least 18 to join
+            dob = (now - datetime.timedelta(days=int(max(365.25*18, (numpy.random.normal(mean_age, std_age)))))).strftime(dt_fmt)
+            # three values + missing
+            gender = numpy.random.choice(['M', 'F', 'O'], p=gender_rates)
+            income = None
+            for i in range(25):
+                x = max(25000, numpy.random.exponential(beta))
+                if min_income <= x <= max_income:
+                    income = x
+                    break
+        else:
+            dob = None
+            gender = None
+            income = None
+
+        person_view_offer_sensitivity = Categorical(['background', 'offer_age', 'web', 'email', 'mobile', 'social'],
+                                                    [g_inv(0.50) - 4, -abs(g_inv(0.01) / float(1 * 24 * 30)), 1, 1, 1,
+                                                     1])
+        person_make_purchase_sensitivity = Categorical(
+            ['background', 'time_since_last_transaction', 'last_viewed_offer_strength', 'viewed_active_offer'],
+            [g_inv(1.0 / 48.0), abs(g_inv(0.10) / float(1 * 24 * 30)), 1, 1])
+        person_purchase_amount_sensitivity = Categorical(
+            ['background', 'income_adjusted_purchase_sensitivity', 'front page', 'local', 'entertainment', 'sports',
+             'opinion', 'comics', 'sweet', 'sour', 'salty', 'bitter', 'umami'], [1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0])
+
+        people.append(Person(became_member_on,
+                             dob=dob,
+                             gender=gender,
+                             income=income,
+                             view_offer_sensitivity=person_view_offer_sensitivity,
+                             make_purchase_sensitivity=person_make_purchase_sensitivity,
+                             purchase_amount_sensitivity=person_purchase_amount_sensitivity))
 
     return people
 
 
 def create_portfolio():
-    offer_a = Offer(0)
-    offer_b = Offer(1)
+    offer_channel = Categorical(('web', 'email', 'mobile', 'social'), (1, 1, 1, 0))
+    offer_type = Categorical(('bogo', 'discount', 'informational'), (0, 1, 0))
+    discount_a = Offer(0, valid_from=0, valid_until=7*24, difficulty=5, reward=2, channel=offer_channel, offer_type=offer_type)
 
-    portfolio = (offer_a, offer_b)
+    offer_channel = Categorical(('web', 'email', 'mobile', 'social'), (1, 1, 1, 1))
+    offer_type = Categorical(('bogo', 'discount', 'informational'), (0, 1, 0))
+    discount_b = Offer(0, valid_from=7*24, valid_until=2*7*24, difficulty=5, reward=2, channel=offer_channel, offer_type=offer_type)
+
+    offer_channel = Categorical(('web', 'email', 'mobile', 'social'), (1, 1, 1, 1))
+    offer_type = Categorical(('bogo', 'discount', 'informational'), (0, 0, 1))
+    info_a = Offer(0, valid_from=2*7*24, valid_until=4*7*24, difficulty=0, reward=0, channel=offer_channel, offer_type=offer_type)
+
+    portfolio = (discount_a, discount_b, info_a)
 
     return portfolio
 
 
-def assign_offers(population, control_fraction=0.5, delimiter='|', clean_path=True):
+def mkdir_if_missing(path):
+    if not os.path.isdir(path):
+        if os.path.exists(path):
+            raise ValueError('ERROR - the path name {} is already in use by a file or link.'.format(path))
+        else:
+            os.makedirs(path)
+
+
+def assign_offers(population, delivery_log_path, control_fraction=0.25, delimiter='|', clean_path=True):
 
     if clean_path:
         data_file_names = glob.glob(os.path.join(population.deliveries_path, '*'))
         for data_file_name in data_file_names:
             os.remove(data_file_name)
 
-    deliveries_file_name = os.path.join(population.deliveries_path, 'test_deliveries.csv')
+    deliveries_short_file_name = 'test_deliveries.csv'
+    deliveries_file_name = os.path.join(population.deliveries_path, deliveries_short_file_name)
+    deliveries_log_file_name = os.path.join(delivery_log_path, deliveries_short_file_name)
+
+    mkdir_if_missing(population.deliveries_path)
+    mkdir_if_missing(delivery_log_path)
 
     offer_ids = population.portfolio.keys()
     deliveries = list()
@@ -66,12 +177,20 @@ def assign_offers(population, control_fraction=0.5, delimiter='|', clean_path=Tr
         for delivery in deliveries:
             print >> deliveries_file, delimiter.join(map(str, delivery))
 
+    # make a copy of the delivery file
+    shutil.copy(deliveries_file_name, deliveries_log_file_name)
+
 
 def main():
-    world = World(real_time_tick=0.000)
-    people = create_people(1000)
+    world = World(real_time_tick=0.000, world_time_tick=6)
+
+    people = list()
+    people.extend(create_people_0(600))
+    people.extend(create_people_1(400))
+
     portfolio = create_portfolio()
 
+    delivery_log_path = 'data/delivery_log'
     deliveries_path = 'data/delivery'
     transcripts_file_name = 'data/transcript.json'
 
@@ -87,9 +206,9 @@ def main():
     with open('data/population.json', 'w') as population_file:
         print >> population_file, population.to_json()
 
-    deliveries = assign_offers(population)
+    deliveries = assign_offers(population, delivery_log_path)
 
-    population.simulate(n_ticks=5)
+    population.simulate(n_ticks=4*30)
 
     return 0
 
